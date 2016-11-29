cca701a800d87baaf4102621814bfaab5015f64c
rishabh-90 <rishabhaggarwal@outlook.com>
2016-11-29 12:15:08 -0400

    Test

---
+++ app/src/main/java/ca/dal/cs/scavenger/Constants.java
@@ -0,0 +1,9 @@
+package ca.dal.cs.scavenger;
+
+/**
+ * Created by Belal on 6/10/2016.
+ */
+public class Constants {
+    public static final String UPLOAD_URL = "http://scavenger.labsrishabh.com/image-code/upload.php";
+    public static final String IMAGES_URL = "http://scavenger.labsrishabh.com/image-code/getImages.php";
+}
