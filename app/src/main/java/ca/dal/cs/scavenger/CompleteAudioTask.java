5b9de17f1faa186df30fce220669f8ed4a5da495
rishabh-90 <rishabhaggarwal@outlook.com>
2016-12-04 20:25:41 -0400

    Sound

--- app/src/main/java/ca/dal/cs/scavenger/CompleteAudioTask.java
+++ app/src/main/java/ca/dal/cs/scavenger/CompleteAudioTask.java
@@ -1,6 +1,8 @@
 package ca.dal.cs.scavenger;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
@@ -9,7 +11,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
 
 import java.io.IOException;
 
@@ -18,65 +25,129 @@ public class CompleteAudioTask extends Activity {
 
     MediaRecorder myAudioRecorder;
     private String outputFile = null;
-    private Button start, stop, play;
+    private ImageButton record, stop, play, save;
 
     private String userid = "1";
     private String taskid = "1";
-
+    private Task mTask;
+    private int mTaskIndex = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_complete_audio_task);
-
-        start = (Button) findViewById(R.id.button1);
-        stop = (Button) findViewById(R.id.button2);
-        play = (Button) findViewById(R.id.button3);
-        stop.setEnabled(false);
-        play.setEnabled(false);
-
-    }
-
-    public void start(View v) {
-        //outputFile = Environment.getExternalStorageDirectory().getAbsolutePath()
-          //      + "/myrec.3gp";
-
-        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath()
-                + "/replacewith_userid_taskid.3gp";
-        myAudioRecorder = new MediaRecorder();
-        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-        myAudioRecorder.setOutputFile(outputFile);
         try {
-            myAudioRecorder.prepare();
-            myAudioRecorder.start();
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_complete_audio_task);
+
+            Intent intent = getIntent();
+            Bundle bundle = intent.getExtras();
+            mTask = bundle.getParcelable("task");
+            mTaskIndex = bundle.getInt("taskIndex");
+            TextView description = (TextView) findViewById(R.id.description);
+            description.setText(mTask.description);
+
+            ImageButton recordButton = (ImageButton) findViewById(R.id.recordButton);
+            recordButton.setImageDrawable(new IconicsDrawable(this)
+                    .icon(GoogleMaterial.Icon.gmd_mic));
+
+            ImageButton stopButton = (ImageButton) findViewById(R.id.stopButton);
+            stopButton.setImageDrawable(new IconicsDrawable(this)
+                    .icon(GoogleMaterial.Icon.gmd_stop));
+
+            ImageButton playButton = (ImageButton) findViewById(R.id.playButton);
+            playButton.setImageDrawable(new IconicsDrawable(this)
+                    .icon(GoogleMaterial.Icon.gmd_play_arrow));
+
+            ImageButton saveButton = (ImageButton) findViewById(R.id.saveButton);
+            saveButton.setImageDrawable(new IconicsDrawable(this)
+                    .icon(GoogleMaterial.Icon.gmd_save));
+
+
+            record = (ImageButton) findViewById(R.id.recordButton);
+            stop = (ImageButton) findViewById(R.id.stopButton);
+            play = (ImageButton) findViewById(R.id.saveButton);
+            save = (ImageButton) findViewById(R.id.saveButton);
+            stop.setEnabled(false);
+            play.setEnabled(false);
+            save.setEnabled(false);
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
+    }
 
-        start.setEnabled(false);
-        stop.setEnabled(true);
-
-        Toast.makeText(this, "Recording started", Toast.LENGTH_SHORT).show();
+    public void record(View v) {
+        //outputFile = Environment.getExternalStorageDirectory().getAbsolutePath()
+          //      + "/myrec.3gp";
+        try {
+            outputFile = Environment.getExternalStorageDirectory().getAbsolutePath()
+                    + "/replacewith_userid_taskid.3gp";
+            myAudioRecorder = new MediaRecorder();
+            myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+            myAudioRecorder.setOutputFile(outputFile);
+            try {
+                myAudioRecorder.prepare();
+                myAudioRecorder.start();
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            record.setEnabled(false);
+            stop.setEnabled(true);
+            Toast.makeText(this, "Recording started", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public void stop(View v) {
-        myAudioRecorder.stop();
-        myAudioRecorder.release();
-        myAudioRecorder = null;
-        stop.setEnabled(false);
-        play.setEnabled(true);
-        Toast.makeText(this, "Audio recorded successfully", Toast.LENGTH_SHORT).show();
+        try {
+            myAudioRecorder.stop();
+            myAudioRecorder.release();
+            myAudioRecorder = null;
+            stop.setEnabled(false);
+            play.setEnabled(true);
+            save.setEnabled(true);
+            Toast.makeText(this, "Audio recorded successfully", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public void play(View v) throws IOException {
+        try {
+            MediaPlayer m = new MediaPlayer();
+            m.setDataSource(outputFile);
+            m.prepare();
+            m.start();
+            record.setEnabled(true);
+            Toast.makeText(this, "Playing audio", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
-    public void play(View v) throws IOException {
-        MediaPlayer m = new MediaPlayer();
-        m.setDataSource(outputFile);
-        m.prepare();
-        m.start();
-        start.setEnabled(true);
-        Toast.makeText(this, "Playing audio", Toast.LENGTH_SHORT).show();
+    public void save(View v) throws IOException {
+        try {
+            mTask.localDataPath = outputFile;
+            Bundle bundle = new Bundle();
+            bundle.putInt("taskIndex", mTaskIndex);
+            bundle.putParcelable("task", mTask);
+            Intent intent = new Intent();
+            intent.putExtras(bundle);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
